675677b23fe744504d9974fa83c0b9cbb779b3b6
fvi_att <jshimizujp@gmail.com>
2015-12-29 16:03:34 +0900

    [feature] create StartPoint and ExitPoint

--- html/js/Evaluate_0001.js
+++ html/js/Evaluate_0001.js
@@ -1,14 +1,14 @@
 function Evaluate(target) {
 
 
-    //コンテンツの結果に関する初期設定
+    //コンテンツの結果に関する
     var result_obj ={
 	result:false,
 	message:document,
 	feedback:""
     };
 
-    //コンテンツの評価を行う
+    //ユーザの解答に対して評価を行う
     if (target["arrow2"] == "drag3"){
 	if (target["arrow1"] == "drag1"){
 	    result_obj.result = true;
