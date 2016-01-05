f791257edd8e4567a8235241eaf216738f7a7f53
fvi_att <jshimizujp@gmail.com>
2016-01-05 16:14:09 +0900

    [feature] とりあえず原始的な一連の流れが完成

--- html/js/Evaluate_0001.js
+++ html/js/Evaluate_0001.js
@@ -10,8 +10,13 @@ function Evaluate(target) {
 
     //ユーザの解答に対して評価を行う
     if (target["arrow2"] == "drag3"){
+
+	//SQLを選択する部分を間違えている
+	result_obj.feedback = "<p>データベースからデータを取り出すためには何が必要でしょうか？</p>";
+	
 	if (target["arrow1"] == "drag1"){
 	    result_obj.result = true;
+	    result_obj.feedback = '<p>これで全てのレクチャーは修了です！お疲れ様でした！　アンケートは必ず答えてください！</p><a href="questionaries.html">アンケートを応える</a>';
 	}
     }
     
--- html/js/Evaluate_0001a.js
+++ html/js/Evaluate_0001a.js
@@ -5,14 +5,16 @@ function Evaluate(target) {
     var result_obj ={
 	result:false,
 	message:document,
-	feedback:""
+	feedback:'<p>これはフィードバックです</p><a href="index.html">次へいく</a>'
     };
 
     //ユーザの解答に対して評価を行う
     if (target["arrow1"] == "drag1"){
 	    result_obj.result = true;
 	
+    }else {
+	result_obj.result = false;
+	result_obj.feedback = "最初にやることは何でしょう？";
     }
-    
     return result_obj;
 }
