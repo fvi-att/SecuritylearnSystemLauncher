e180af9c63eaa5990eb385538e77391a0a892893
fvi_att <jshimizujp@gmail.com>
2016-01-15 14:49:41 +0900

    [feature] step on debugger

--- html/js/Evaluate_0001.js
+++ html/js/Evaluate_0001.js
@@ -1,11 +1,25 @@
 /*
 擬似デバッガシステムの動きを定義するオブジェクトの生成
+processの内部要素の順列にそって実行されます
+idはどの要素をアクティブにして動作させるか
+actionはその要素でどのような動作を行うかを提議させます
 */
 function getDebuggerProcess() {
 
     var process_obj = {
 	process:[
-	    {id:"drop1"},{id:"arrow1"},{id:"drop2"},{id:"arrow2"},{id:"drop3"}
+	    {id:"drop1", action:function(){
+		alert("you run step1");
+
+	    }},
+	    {id:"arrow1", action:function() {
+	    }},
+	    {id:"drop2", action:function() {
+	    }},
+	    {id:"arrow2",action:function() {
+	    }},
+	    {id:"drop3", action:function() {
+	    }}
 	]
     }
 
