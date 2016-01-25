2f3e961f3b1702bf826826f1dcd5f573e9be1a71
fvi_att <jshimizujp@gmail.com>
2016-01-25 16:24:38 +0900

    [feature] HTMLデータとコードの分離

---
+++ html/js/debugger.js
@@ -0,0 +1,66 @@
+// intilized nums
+  var user_answers = new Object();
+  var process_step_cnt = 0;
+
+  
+$(function() {
+// 1ドラッグ＆ドロップ機能を実装
+  $(".dragbox").draggable();
+  $(".drop").droppable({
+    drop: function(e,ui) {
+     
+    }
+   });
+　$(".arrow").droppable({
+     drop: function(e,ui){
+
+    user_answers[$(this).attr("id")] = ui.draggable.attr("id");
+    //alert(user_answers.toSource());
+    
+  }});
+
+
+
+/* 擬似デバッガを操作*/
+$("#btn_start").click(function(){
+
+  process_obj = getDebuggerProcess(user_answers);
+  processes = process_obj["process"];
+
+
+
+  /* 変更している部分を表示する　*/
+$("#" + processes[process_step_cnt]["id"] ).css('border-color','red');
+$("#" + processes[process_step_cnt]["id"] ).css('border-width','5px');
+
+ /* 前のステップの部分を青で表示するようにする */
+if (process_step_cnt > 0){
+　　$("#" + processes[process_step_cnt -1]["id"] ).css('border-color','blue');
+　　$("#" + processes[process_step_cnt -1]["id"] ).css('border-width','2px');
+}
+
+
+/*擬似デバッガ内での処理を行うステップ */
+processes[process_step_cnt]["action"]();
+
+
+/*フィードバックを返すかどうかの処理 */
+if (!processes[process_step_cnt]["evaluate"]() || (process_step_cnt == processes.length -1)) {
+    $("#right_message").load(processes[process_step_cnt]["feedback"]);
+}
+
+
+//ステップを進める
+process_step_cnt++;
+
+//ステップ数の表示
+$("#step_cnt").text(process_step_cnt);
+
+
+});
+// 擬似デバッガステップ終了
+
+
+
+
+});
