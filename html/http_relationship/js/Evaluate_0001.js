d8d501460bdaf2f07f16063a7de3dc9b0df3ac84
fvi_att <jshimizujp@gmail.com>
2016-01-25 15:57:52 +0900

    [feature]逐次検証型デバッガの試験的実装

--- html/http_relationship/js/Evaluate_0001.js
+++ html/http_relationship/js/Evaluate_0001.js
@@ -4,44 +4,38 @@ processの内部要素の順列にそって実行されます
 idはどの要素をアクティブにして動作させるか
 actionはその要素でどのような動作を行うかを提議させます
 */
-function getDebuggerProcess() {
+function getDebuggerProcess(target) {
 
     var process_obj = {
+
 
 	process:[
 	    {id:"drop1", action:function(){
 		//id=drop1の処理が通過するとき、このfunction内の処理が実行されます
-
-	    }},
+	    }, evaluate:function(){return true;}
+	     ,feedback:"feedback/exercise01/http_wrong.html"},
+	    
 	    {id:"arrow1", action:function() {
 		//id=arrow1の処理が通過するとき、このfunction内の処理が実行されます
-	    }},
+	    }, evaluate:function(){
+		if(target["arrow1"] == "drag1"){return true;}else{return false;}
+	    },feedback:"feedback/exercise01/http_wrong.html"},
+	    
 	    {id:"drop2", action:function() {
 		//id=drop2の処理が通過するとき、このfunction内の処理が実行されます
-	    }},
+	    }, evaluate:function(){return true;},feedback:"feedback/exercise01/http_wrong.html"},
 	    {id:"arrow2",action:function() {
 		//id=arrow2の処理が通過するとき、このfunction内の処理が実行されます
-	    }},
+	    }, evaluate:function(){return true;},feedback:"feedback/exercise01/http_wrong.html"},
 	    {id:"drop3", action:function() {
 		//id=drop3の処理が通過するとき、このfunction内の処理が実行されます
-	    }}
+	    }, evaluate:function(){return true;},feedback:"feedback/exercise01/correct.html"},
 	]
     }
 
     return process_obj;
 }
 
-/* シミュレーションの各ステップごとにて評価を行い、正解不正解、oフィードバックを返すようにする*/
-function getStepEvaluateObj(){
-    var eval_obj = [
-	{evaluate:function(){ return true;},correct_fb:"",wrong_fb:""},
-	{evaluate:function(){ return true;},correct_fb:"",wrong_fb:""},
-	{evaluate:function(){ return true;},correct_fb:"",wrong_fb:""},
-	{evaluate:function(){ return true;},correct_fb:"",wrong_fb:""},
-	{evaluate:function(){ return true;},correct_fb:"",wrong_fb:""},
-    ];
-    return eval_obj;
-}
 
 
 
