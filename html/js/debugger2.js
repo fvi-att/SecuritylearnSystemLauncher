7af9167ac98ce9e128a709adb505789ed9f716b9
Shimizu Ryosuke <shimizu.r.ad@m.titech.ac.jp>
2016-01-27 17:01:01 +0900

    [feature]表示変更

--- html/js/debugger2.js
+++ html/js/debugger2.js
@@ -2,9 +2,7 @@
   var user_answers = new Object();
   var process_step_cnt = 0;
   var result_flag = true;
-  var step_callback = function(){};
-
-
+  
 $(function() {
 // 1ドラッグ＆ドロップ機能を実装
   $(".dragbox").draggable();
@@ -49,8 +47,6 @@ if (process_step_cnt > 0){
 /*擬似デバッガ内での処理を行うステップ */
 processes[process_step_cnt]["action"]();
 
-//callback呼び出し
-    step_callback(user_answers);
 
 /*フィードバックを返すかどうかの処理 */
 if (!processes[process_step_cnt]["evaluate"]() || (process_step_cnt == processes.length -1)) {
