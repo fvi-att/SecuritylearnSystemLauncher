388bd4b214d0285fe7b7a62b7730f37172489e08
Shimizu Ryosuke <shimizu.r.ad@m.titech.ac.jp>
2016-01-27 13:32:24 +0900

    [feature]フィードバック部分におけるページ遷移によるシナリオの変更

--- html/http_relationship/js/Evaluate_0002.js
+++ html/http_relationship/js/Evaluate_0002.js
@@ -13,44 +13,44 @@ function getDebuggerProcess(target) {
 	    {id:"drop1", action:function(){
 		//id=drop1の処理が通過するとき、このfunction内の処理が実行されます
 	    }, evaluate:function(){return true;}
-	     ,feedback:"feedback/exercise01/http_wrong.html"},
+	     ,feedback:"feedback/exercise02/http_wrong.html"},
 	    
 	    {id:"arrow1", action:function() {
 		//id=arrow1の処理が通過するとき、このfunction内の処理が実行されます
 	    }, evaluate:function(){
 		if(target["arrow1"] == "drag1"){return true;}else{return false;}
-	    },feedback:"feedback/exercise01/http_wrong.html"},
+	    },feedback:"feedback/exercise02/http_wrong.html"},
 	    
 	    {id:"drop2", action:function() {
 		//id=drop2の処理が通過するとき、このfunction内の処理が実行されます
-	    }, evaluate:function(){return true;},feedback:"feedback/exercise01/http_wrong.html"},
+	    }, evaluate:function(){return true;},feedback:"feedback/exercise02/http_wrong.html"},
 	    
 	    {id:"arrow2",action:function() {
 		//id=arrow2の処理が通過するとき、このfunction内の処理が実行されます
 	    }, evaluate:function(){if(target["arrow2"] == "drag3") {return true;}else{return false;}
-	      },feedback:"feedback/exercise01/http_wrong.html"},
+	      },feedback:"feedback/exercise02/http_wrong.html"},
 	    
 	    {id:"drop3", action:function() {
 		//id=drop3の処理が通過するとき、このfunction内の処理が実行されます
-	    }, evaluate:function(){return true;},feedback:"feedback/exercise01/correct.html"},
+	    }, evaluate:function(){return true;},feedback:"feedback/exercise02/correct.html"},
 
 	    {id:"arrow3",action:function() {
-		//id=arrow2の処理が通過するとき、このfunction内の処理が実行されます
+		//id=arrow3の処理が通過するとき、このfunction内の処理が実行されます
 	    }, evaluate:function(){if(target["arrow3"] == "drag6") {return true;}else{return false;}
-	      },feedback:"feedback/exercise01/http_wrong.html"},
+	      },feedback:"feedback/exercise02/http_wrong.html"},
 
 	    {id:"drop2", action:function() {
 		//id=drop2の処理が通過するとき、このfunction内の処理が実行されます
-	    }, evaluate:function(){return true;},feedback:"feedback/exercise01/http_wrong.html"},
+	    }, evaluate:function(){return true;},feedback:"feedback/exercise02/http_wrong.html"},
 
 	    {id:"arrow4",action:function() {
-		//id=arrow2の処理が通過するとき、このfunction内の処理が実行されます
+		//id=arrow4の処理が通過するとき、このfunction内の処理が実行されます
 	    }, evaluate:function(){if(target["arrow4"] == "drag2") {return true;}else{return false;}
-	      },feedback:"feedback/exercise01/http_wrong.html"},
+	      },feedback:"feedback/exercise02/http_wrong.html"},
 
 	    {id:"drop1", action:function() {
-		//id=drop3の処理が通過するとき、このfunction内の処理が実行されます
-	    }, evaluate:function(){return true;},feedback:"feedback/exercise01/correct.html"},
+		//id=drop1の処理が通過するとき、このfunction内の処理が実行されます
+	    }, evaluate:function(){return true;},feedback:"feedback/exercise02/correct.html"},
 
 	    
 	]
