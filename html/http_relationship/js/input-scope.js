5e0d07bd7c9e5f9f9d51a92b1c27405038d0d194
Shimizu Ryosuke <shimizu.r.ad@m.titech.ac.jp>
2016-01-29 12:42:24 +0900

    [feature] シミュレーション部分追加

---
+++ html/http_relationship/js/input-scope.js
@@ -0,0 +1,17 @@
+var input_obj =
+    {input1:"GET login.html",
+     input2:"200 OK",
+     input3:"SELECT USERNAME FROM USER_TABLE WHERE ID=&quot;&quot;",
+     input4:"userid=Bob&password=passworddesu",
+     input5:"SELECT USERNAME FROM USER_TABLE WHERE ID=&quot;admin&quot;",
+     input6:"{status:'success',userid:'Bob'",
+    input7:"&lt;html&gh;</br>&lt;body&gt;ようこそ　ログイン完了・・・"
+}
+
+
+var scope_obj =
+    {dragdrop1:"scope-input1",
+     dragdrop3:"scope-input2",
+     dragdrop6:"scope-input3",
+     dragdrop2:"scope-input4"
+    }
