73efe50b4c629d1b6cfb68fb370e8f4f9d3b64e0
fvi_att <jshimizujp@gmail.com>
2016-01-29 17:55:10 +0900

    [feature]擬似ブラウザを用いた認証システム

--- html/sqli/js/Evaluate_0001.js
+++ html/sqli/js/Evaluate_0001.js
@@ -12,18 +12,20 @@ function getDebuggerProcess(target) {
 	process:[
 	    {id:["drop1"], action:function(){
 		//id=drop1の処理が通過するとき、このfunction内の処理が実行されます
+		$("#scope-input1").text("id="+$("#psuedo-loginform [name=id-form]").val()+"&password="+$("#psuedo-loginform [name=pw-form]").val());
+		$("#scope-input1").fadeOut(200,function(){$(this).fadeIn(200)});
 		
 	    }, evaluate:function(){return true;}
 	     ,feedback:"feedback/exercise03/http_wrong.html"},
 	    
 	    {id:["drag1","scope1"], action:function() {
 		//id=arrow1の処理が通過するとき、このfunction内の処理が実行されます
-
+		
 	    }, evaluate:function(){return true;},feedback:"feedback/exercise03/http_wrong.html"},
 	    
 	    {id:["drop2"], action:function() {
 		//id=drop2の処理が通過するとき、このfunction内の処理が実行されます
-
+		$("#scope-input2").text("ID='" + $("#psuedo-loginform [name=id-form]").val() + "' AND PASSWORD='" + $("#psuedo-loginform [name=pw-form]").val()+"'");
 	    }, evaluate:function(){return true;},feedback:"feedback/exercise03/http_wrong.html"},
 	    
 	    {id:["drag3","scope2"],action:function() {
@@ -48,12 +50,12 @@ function getDebuggerProcess(target) {
 
 	    {id:["drag2","scope4"],action:function() {
 		//id=arrow4の処理が通過するとき、このfunction内の処理が実行されます
-
+		$("#scope-input4").text("こんにちは"+$("#psuedo-loginform [name=id-form]").val()+ "様");
 	    }, evaluate:function(){return true;},feedback:"feedback/exercise03/http_wrong.html"},
 
 	    {id:"drop1", action:function() {
 		//id=drop1の処理が通過するとき、このfunction内の処理が実行されます
-
+		$("#psuedo-browser-content").html("<h1>ログイン完了！</h1><p>こんにちは"+$("#psuedo-loginform [name=id-form]").val()+ "様</p>");
 	    }, evaluate:function(){return true;},feedback:"feedback/exercise03/correct.html"},
 
 	    
