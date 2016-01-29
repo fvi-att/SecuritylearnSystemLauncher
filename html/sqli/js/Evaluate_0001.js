d61c5bee506042fd339df6b84613f389b40410a4
fvi_att <jshimizujp@gmail.com>
2016-01-29 18:01:22 +0900

    [fix] error path fix

--- html/sqli/js/Evaluate_0001.js
+++ html/sqli/js/Evaluate_0001.js
@@ -16,47 +16,47 @@ function getDebuggerProcess(target) {
 		$("#scope-input1").fadeOut(200,function(){$(this).fadeIn(200)});
 		
 	    }, evaluate:function(){return true;}
-	     ,feedback:"feedback/exercise03/http_wrong.html"},
+	     ,feedback:"feedback/exercise01/http_wrong.html"},
 	    
 	    {id:["drag1","scope1"], action:function() {
 		//id=arrow1の処理が通過するとき、このfunction内の処理が実行されます
 		
-	    }, evaluate:function(){return true;},feedback:"feedback/exercise03/http_wrong.html"},
+	    }, evaluate:function(){return true;},feedback:"feedback/exercise01/http_wrong.html"},
 	    
 	    {id:["drop2"], action:function() {
 		//id=drop2の処理が通過するとき、このfunction内の処理が実行されます
 		$("#scope-input2").text("ID='" + $("#psuedo-loginform [name=id-form]").val() + "' AND PASSWORD='" + $("#psuedo-loginform [name=pw-form]").val()+"'");
-	    }, evaluate:function(){return true;},feedback:"feedback/exercise03/http_wrong.html"},
+	    }, evaluate:function(){return true;},feedback:"feedback/exercise01/http_wrong.html"},
 	    
 	    {id:["drag3","scope2"],action:function() {
 		//id=arrow2の処理が通過するとき、このfunction内の処理が実行されます
 
-	    }, evaluate:function(){return true;},feedback:"feedback/exercise03/http_wrong.html"},
+	    }, evaluate:function(){return true;},feedback:"feedback/exercise01/http_wrong.html"},
 	    
 	    {id:["drop3"], action:function() {
 		//id=drop3の処理が通過するとき、このfunction内の処理が実行されます
 
-	    }, evaluate:function(){return true;},feedback:"feedback/exercise03/correct.html"},
+	    }, evaluate:function(){return true;},feedback:"feedback/exercise01/correct.html"},
 
 	    {id:["drag6","scope3"],action:function() {
 		//id=arrow3の処理が通過するとき、このfunction内の処理が実行されます
 
-	    }, evaluate:function(){return true;},feedback:"feedback/exercise03/http_wrong.html"},
+	    }, evaluate:function(){return true;},feedback:"feedback/exercise01/http_wrong.html"},
 
 	    {id:["drop2"], action:function() {
 		//id=drop2の処理が通過するとき、このfunction内の処理が実行されます
 
-	    }, evaluate:function(){return true;},feedback:"feedback/exercise03/http_wrong.html"},
+	    }, evaluate:function(){return true;},feedback:"feedback/exercise01/http_wrong.html"},
 
 	    {id:["drag2","scope4"],action:function() {
 		//id=arrow4の処理が通過するとき、このfunction内の処理が実行されます
 		$("#scope-input4").text("こんにちは"+$("#psuedo-loginform [name=id-form]").val()+ "様");
-	    }, evaluate:function(){return true;},feedback:"feedback/exercise03/http_wrong.html"},
+	    }, evaluate:function(){return true;},feedback:"feedback/exercise01/http_wrong.html"},
 
 	    {id:"drop1", action:function() {
 		//id=drop1の処理が通過するとき、このfunction内の処理が実行されます
 		$("#psuedo-browser-content").html("<h1>ログイン完了！</h1><p>こんにちは"+$("#psuedo-loginform [name=id-form]").val()+ "様</p>");
-	    }, evaluate:function(){return true;},feedback:"feedback/exercise03/correct.html"},
+	    }, evaluate:function(){return true;},feedback:"feedback/exercise01/correct.html"},
 
 	    
 	]
