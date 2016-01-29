80d02038a46a46b04ac99e31ff0896b4d64d44a2
Shimizu Ryosuke <shimizu.r.ad@m.titech.ac.jp>
2016-01-30 01:41:18 +0900

    [feature]すべて正解の場合のストーリーの実装

--- html/http_relationship/js/input-scope.js
+++ html/http_relationship/js/input-scope.js
@@ -1,10 +1,10 @@
 var input_obj =
-    {input1:"GET login.html",
+    {input1:"POST /login/",
      input2:"200 OK",
-     input3:"SELECT USERNAME FROM USER_TABLE WHERE ID=&quot;&quot;",
-     input4:"userid=Bob&password=passworddesu",
+     input3:"WHERE ID='Bob' AND PASSWORD='passworddesu';",
+     input4:"id=Bob&password=passworddesu",
      input5:"SELECT USERNAME FROM USER_TABLE WHERE ID=&quot;admin&quot;",
-     input6:"{status:'success',userid:'Bob'",
+     input6:"{status:'success',userid:'Bob'}",
     input7:"&lt;html&gh;</br>&lt;body&gt;ようこそ　ログイン完了・・・"
 }
 
